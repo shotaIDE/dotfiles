0cc95683fd121d0fa393198569c84a7ec0e31593
Shota IDE <ide.shota.dev@gmail.com>
2017-01-13 19:08:45 +0900

    [fix] 元々のディレクトリを変更

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
 home_dir='/Users/ide'
-dotfiles_dir=$home_dir/'dotfiles'
+dotfiles_dir=$home_dir'/programs/dotfiles'
 
 ln -sf $dotfiles_dir/.emacs $home_dir/.emacs
 ln -sf $dotfiles_dir/.profile $home_dir/.profile
