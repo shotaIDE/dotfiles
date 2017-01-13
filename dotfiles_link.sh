ba4ac618a90fb2bca837ee03429e30c165a7e5a3
Shota IDE <ide.shota.dev@gmail.com>
2017-01-13 20:42:39 +0900

    [add] Git関連のドットファイルを追加

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -3,5 +3,7 @@
 home_dir='/Users/ide'
 dotfiles_dir=$home_dir'/programs/dotfiles'
 
-ln -sf $dotfiles_dir/.emacs $home_dir/.emacs
-ln -sf $dotfiles_dir/.profile $home_dir/.profile
+ln -sf $dotfiles_dir/emacs $home_dir/.emacs
+ln -sf $dotfiles_dir/profile $home_dir/.profile
+ln -sf $dotfiles_dir/gitconfig $home_dir/.gitconfig
+ln -sf $dotfiles_dir/gitignore $home_dir/.gitignore
