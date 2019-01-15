55cb26cca18d5f5b69bb30ab192914ad23863739
Shota Ide <shota.ide@daj.co.jp>
2019-01-15 15:29:57 +0900

    修飾キーのマッピング設定を追加

--- bin/macos_defaults.sh
+++ bin/macos_defaults.sh
@@ -4,6 +4,47 @@ defaults write com.apple.dock autohide -int 1
 # Yen key sends backslash
 defaults write com.apple.inputmethod.Kotoeri JIMPrefCharacterForYenKey -int 1
 
+# Modifier key mappings
+keyboard_id=$(ioreg -c AppleEmbeddedKeyboard -r | grep -Ei "VendorID|ProductID|VendorIDSource" | awk '{print $4}' | paste -s -d'-\n' - | paste -s -d'-\n' -)
+# CapsLock -> Control
+defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add '
+<dict>
+  <key>HIDKeyboardModifierMappingDst</key>
+  <integer>30064771300</integer>
+  <key>HIDKeyboardModifierMappingSrc</key>
+  <integer>30064771129</integer>
+</dict>'
+# Option -> Command
+defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add '
+<dict>
+  <key>HIDKeyboardModifierMappingDst</key>
+  <integer>30064771299</integer>
+  <key>HIDKeyboardModifierMappingSrc</key>
+  <integer>30064771302</integer>
+</dict>'
+defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add '
+<dict>
+  <key>HIDKeyboardModifierMappingDst</key>
+  <integer>30064771303</integer>
+  <key>HIDKeyboardModifierMappingSrc</key>
+  <integer>30064771298</integer>
+</dict>'
+# Command -> Option
+defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add '
+<dict>
+  <key>HIDKeyboardModifierMappingDst</key>
+  <integer>30064771298</integer>
+  <key>HIDKeyboardModifierMappingSrc</key>
+  <integer>30064771303</integer>
+</dict>'
+defaults -currentHost write -g com.apple.keyboard.modifiermapping.${keyboard_id} -array-add '
+<dict>
+  <key>HIDKeyboardModifierMappingDst</key>
+  <integer>30064771302</integer>
+  <key>HIDKeyboardModifierMappingSrc</key>
+  <integer>30064771299</integer>
+</dict>'
+
 # Enable right click
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode TwoButton
 
